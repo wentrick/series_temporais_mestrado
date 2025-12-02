a625bd721ebdb885d40cc7a9f87dbe45ddc08561
Wentrick <davi.w.feijo@gmail.com>
2025-12-02 18:00:03 -0300

    detalhes extras

--- Trabalho/trabalho_2/proposta_dados.R
+++ Trabalho/trabalho_2/proposta_dados.R
@@ -1,4 +1,4 @@
-pacman::p_load(tidyverse, lubridate, rbcb, fpp3, jsonlite, tidyquant, tsibble, purrr, forecast)
+pacman::p_load(tidyverse, lubridate, rbcb, fpp3, jsonlite, tidyquant, tsibble, purrr, forecast, zoo)
 
 
 # Ticker do USD/BRL no Yahoo: "BRL=X"
@@ -83,19 +83,57 @@ brent_mstl <- mstl(brent_ts, iterate = 2)
 plot(brent_mstl, main = "Decomposição MSTL - Brent")
 
 
+##### Rodar modelo de Regressao Dinamica
 
 
+base_reg <- base_final %>%
+  select(data,
+         gasolina = preco_medio,
+         dolar,
+         brent) %>%
+  # manter apenas datas onde temos as 3 séries observadas
+  filter(!is.na(gasolina),
+         !is.na(dolar),
+         !is.na(brent)) %>%
+  as_tsibble(index = data)
+
+base_reg
+
+treino <- base_reg %>% filter(data <= as.Date("2023-12-31"))
+teste  <- base_reg %>% filter(data >  as.Date("2023-12-31"))
+
+### Testando Estacionariedade
+
+base_reg$gasolina %>% ndiffs()
+
+base_reg$dolar %>% ndiffs()
+
+base_reg$brent %>% ndiffs()
 
 
+melhor_AICc = Inf
+for(p in 0:10){
+  for(q in 0:10){
+    fit = Arima(y=base_reg$gasolina, xreg=base_reg$dolar, order = c(p, 1, q))
+    if(fit$aicc < melhor_AICc){
+      melhor_AICc = fit$aicc
+      cat("p =",p,", q =",q,", AICc =", fit$aicc, "\n")
+    }
+  }
+}
+
+fit_dm = Arima(y=base_reg$gasolina, xreg=c(base_reg$dolar), order = c(8, 1, 10))
+fit_dm
+
+summary(fit_dm)
+
+E <- fit_dm$residuals %>% as.ts()
+par(mfrow=c(2,2))
+plot(E); acf(E); pacf(E); qqnorm(E); qqline(E)
 
 
-
-
-
-
-
-
-
+E <- fit_dm$residuals %>% as.ts()
+E %>% Box.test(lag=15, type="Ljung-Box")
 
 
 
